69047005bff858a711464614e7de3e6328f9755a
richardbumbury <richard@netlaw.com>
2023-11-27 12:15:15 -0500

    update action interface

--- src/interfaces/action.ts
+++ src/interfaces/action.ts
@@ -1,44 +1,32 @@
-import { Result, Issue } from "../modules";
-
 /**
- * Represents an action that can be performed on a given state.
- *
- * @template T The type of the state on which the action is performed.
- * @template A The type of the action itself, extending the IAction interface.
- * @template R The type of the result of the action, which can be either a Result or an Issue.
- * @template P The type of parameters the action accepts, defaulting to any type.
+ * This interface represents an executable action within an application.
+ * It encapsulates the logic and necessary data to perform a specific operation.
+ * 
+ * @template P The parameters used for the action's execution.
+ * @template R The response produced by executing the action.
  */
-export interface IAction<T, A extends IAction<any, any, any>, R extends Result<T, A> | Issue<T>, P = any> {
+export interface IAction<P = any, R = any> {
     /**
-     * The name or identifier of the action.
+     * The name of the action.
      */
-    method: string;
+    name: string;
 
     /**
-     * The parameters passed to the action. These are used to influence the action's behavior.
+     * The parameters passed to the action.
      */
-    parameters: P;
+    params: P[];
+
+    /**
+     * The function encapsulating the operation the action will execute.
+     * 
+     * @param params The parameters necessary for executing the action.
+     * 
+     * @returns Returns a result of type R, as defined by the action's logic
+     */
+    exec: (params: P[]) => R;
 
     /**
      * The timestamp when the action was created or initialized.
      */
     timestamp: Date;
-
-    /**
-     * Executes the action on a given state. This method should encapsulate the primary logic of the action.
-     * 
-     * @param currentState The current state before the action is applied.
-     * 
-     * @returns The result of the action, which is either a successful result encapsulated in a `Result` object or an error scenario represented by an `Issue`.
-     */
-    invoke(currentState: T): R;
-
-    /**
-     * Reverts the effects of the action on a given state. This is used for undoing the action.
-     * 
-     * @param currentState The current state to which the action's effects should be reverted.
-     * 
-     * @returns The state after undoing the action.
-     */
-    revert(currentState: T): T;
 }
